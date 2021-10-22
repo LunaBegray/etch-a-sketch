c3bd328a9cc72940debf7e524f6dd78356136b08
luna <lunashoval@gmail.com>
2021-10-22 19:23:31 -0400

    script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let wh = 16;
 for(i = 0; i < wh*wh; i++){
     let box = document.createElement('div');
     box.classList.add("gridBoxes");
-    box.addEventListener("click", function(event){
+    box.addEventListener("mouseover", function(event){
         event.target.style.color = "black";
     });
     gridContainer.appendChild(box);
